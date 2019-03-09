ce6f14f60c92c8fcb2d991535e0a6ff1c97d71cb
Marta <43724620+gulidamarta@users.noreply.github.com>
2019-03-09 14:48:56 +0300

    Add files via upload

---
+++ WhoIs/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace WhoIs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxDomain = new System.Windows.Forms.TextBox();
+            this.textBoxInfo = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Domain :";
+            // 
+            // textBoxDomain
+            // 
+            this.textBoxDomain.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxDomain.Location = new System.Drawing.Point(86, 21);
+            this.textBoxDomain.Name = "textBoxDomain";
+            this.textBoxDomain.Size = new System.Drawing.Size(238, 24);
+            this.textBoxDomain.TabIndex = 1;
+            // 
+            // textBoxInfo
+            // 
+            this.textBoxInfo.Location = new System.Drawing.Point(15, 61);
+            this.textBoxInfo.Multiline = true;
+            this.textBoxInfo.Name = "textBoxInfo";
+            this.textBoxInfo.Size = new System.Drawing.Size(620, 377);
+            this.textBoxInfo.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(344, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 32);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Get";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(645, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxInfo);
+            this.Controls.Add(this.textBoxDomain);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "WhoIs Server";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxDomain;
+        private System.Windows.Forms.TextBox textBoxInfo;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WhoIs/WhoIsClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Net.Sockets;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace WhoIs
+{
+    class WhoIsClass
+    {
+        public static string GetInfo(string domain, ResultType type)
+        {
+            TcpClient client = new TcpClient();
+            client.Connect("whois.networksolutions.com", 43);
+
+            domain += "\r\n";
+            byte[] arrDomain = Encoding.UTF8.GetBytes(domain);
+
+            Stream str = client.GetStream();
+            str.Write(arrDomain, 0, domain.Length);
+
+            StreamReader reader = new StreamReader(client.GetStream(), Encoding.UTF8);
+            string info = "";
+
+            if(type == ResultType.TextBox)
+            {
+                info = Regex.Replace(reader.ReadToEnd(), "\n", "\r\n");
+            }
+            else
+            {
+                info = Regex.Replace(reader.ReadToEnd(), "\n", "<br>");
+            }
+            return info;
+        }   
+        
+        public enum ResultType
+        {
+            TextBox, 
+            Web
+        }
+    }
+}
